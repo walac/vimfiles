9aa8b74c9f03d432bf4f593efb31d2108905f436
Wander Lairson Costa <wander.lairson@gmail.com>
2020-06-21 16:33:40 -0300

    Add bootstrap.sh script
    The bootstrap script does basic installation after cloning the repo.

---
+++ bootstrap.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -ve
+
+: WORK=${WORK=$HOME/work}
+
+vim -c :BundleInstall -c :qall
+
+# XXX: How to wait for CocInstall to finish before quitting?
+vim -c ":CocInstall \
+    coc-marketplace \
+    coc-json \
+    coc-cmake \
+    coc-explorer \
+    coc-python \
+    coc-rls \
+    coc-yaml \
+    coc-clangd \
+    coc-tsserver \
+    coc-go \
+    " # -c :qall
+
+vim -c :GoInstallBinaries -c :qall
+
+cd $WORK/vimfiles/bundle/tern_for_vim/
+npm install
+cd -
